6944b4caf8d677b34c2f90e58600d9aef6e3eb5a
Alex WSL <alexdaenkov200@gmail.com>
2024-10-06 23:13:26 +0300

    8th step

--- app/src/main/java/hexlet/code/Games.java
+++ app/src/main/java/hexlet/code/Games.java
@@ -1,9 +1,7 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Games {
-    public static final String[] GAMES = {"Exit", "Greet", "Even", "Calc", "GCD"};
+    public static final String[] GAMES = {"Exit", "Greet", "Even", "Calc", "GCD", "Progression"};
     private static final int minValue = 1;
     private static final int maxValue = 100;
 
@@ -15,13 +13,16 @@ public class Games {
         if (gameNum == 2) return "Answer 'yes' if the number is even, otherwise answer 'no'.";
         else if (gameNum == 3) return "What is the result of the expression?";
         else if (gameNum == 4) return "Find the greatest common divisor of given numbers.";
+        else if (gameNum == 5) return "What number is missing in the progression?";
         return null;
     }
 
     public static String[] getQuestionAndAnswer(int gameNum) {
         if (gameNum == 2) return even();
         else if (gameNum == 3) return calc();
-        else return GSD();
+        else if (gameNum == 4) return gsd();
+        else if (gameNum == 5) return progression();
+        else return new String[] { "", "" };
     }
 
     public static String[] even() {
@@ -60,9 +61,34 @@ public class Games {
         return Math.max(a, b);
     }
 
-    public static String[] GSD() {
+    public static String[] gsd() {
         int a = getRandInt(minValue, maxValue);
         int b = getRandInt(minValue, maxValue);
         return new String[] { a + " " + b, String.valueOf(findGSD(a, b)) };
+    }
+
+    public static String[] progression() {
+        final int progressionLen = 10;
+        final int startValueMin = 1;
+        final int startValueMax = 20;
+        final int startStepMin = 1;
+        final int startStepMax = 8;
+        final int step = getRandInt(startStepMin, startStepMax);
+        final int pointNum = getRandInt(0, progressionLen - 1);
+        int[] progressionArr = new int[progressionLen];
+        progressionArr[0] = getRandInt(startValueMin, startValueMax);
+
+        for (int i = 1; i < progressionLen; i++) {
+            progressionArr[i] = progressionArr[i - 1] + step;
+        }
+
+        StringBuilder question = new StringBuilder();
+        for (int i = 0; i < progressionLen; i++) {
+            if (i == pointNum) question.append("..");
+            else question.append(progressionArr[i]);
+            if (i < progressionLen - 1) question.append(" ");
+        }
+
+        return new String[] { question.toString(), String.valueOf(progressionArr[pointNum])};
     }
 }
